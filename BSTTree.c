5dc7edca632a593bfdd698bd75ce2e624b436dc3
Tejas Shah <tejasshah2k19@gmail.com>
2026-02-17 19:57:42 +0530

    bst add node

--- BSTTree.c
+++ BSTTree.c
@@ -8,33 +8,42 @@ struct node
     struct node *right;
 };
 
+struct node *root = NULL;
+struct node *tmp = NULL;
+
+struct node *addNode(struct node *root, int num) // null,70   70,80  null,80
+{
+    if (root == NULL)
+    {
+
+        root = malloc(sizeof(struct node)); // data , left , right
+        root->data = num;
+        root->left = NULL;
+        root->right = NULL;
+        return root;
+    }
+    else if (num > root->data)
+    {
+        // right
+        root->right = addNode(root->right, num);
+    }
+    else if (num < root->data)
+    {
+        // left
+        root->left = addNode(root->left, num);
+    }
+
+    return root;
+}
+
 int main()
 {
-    struct node *root = NULL;
-    struct node *tmp = NULL;
 
-    root = malloc(sizeof(struct node)); // data , left , right
+    root = addNode(root, 70);
+    root = addNode(root, 80);
+    root = addNode(root, 60);
 
-    root->data = 50;
-    root->left = NULL;
-    root->right = NULL;
-
-    tmp = malloc(sizeof(struct node));
-    tmp->data = 70;
-    tmp->left = NULL;
-    tmp->right = NULL;
-
-    root->right = tmp;
-
-    tmp = malloc(sizeof(struct node));
-    tmp->data = 40;
-    tmp->left = NULL;
-    tmp->right = NULL;
-
-    root->left = tmp;
-
-
-    printf(" %d %d %d ",root->data,root->left->data,root->right->data);
+    printf(" %d %d %d ", root->data, root->left->data, root->right->data); // 70 60 80
 
     return 0;
 }
